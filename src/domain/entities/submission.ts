fc5f4d6620f42b97b20247fea22d663464e58daf
Thiago Mota Santos <lucimara1422@gmail.com>
2022-07-23 14:19:05 -0300

    tdd

---
+++ src/domain/entities/submission.ts
@@ -0,0 +1,20 @@
+import { Entity } from "../../Core/domain/entities";
+
+interface SubmissionProps{
+    challengeId: string;
+    studentId: String;
+    createAt: Date;
+
+}
+
+export class Submission extends Entity<SubmissionProps>{
+     private constructor(props: SubmissionProps, _id?: string){
+        super(props, _id);
+     }
+
+    static create(props: SubmissionProps, id?: string){
+        const submission = new Submission(props, id);
+
+        return Submission;
+    }
+}
